98206ea6877210d70014a5461592e9c8615eff54
Garavirod <rodrigogarciaavila26@gmail.com>
2019-12-18 22:13:56 -0600

    Operador Map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,9 +12,8 @@ export class HomeComponent implements OnInit {
   nuevasCanciones: any [] = [];
 
   constructor(private spotify: SpotifyService) {
-    this.spotify.getNewReleases().subscribe( (data: any) => {
-      console.log(data.albums.items);
-      this.nuevasCanciones = data.albums.items;
+    this.spotify.getNewReleases().subscribe( (data: any) => {      
+      this.nuevasCanciones = data;
     });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 // Se puede hacer uso en otros servicicios, al ser Injectable
 @Injectable({
@@ -12,9 +13,10 @@ export class SpotifyService {
   getNewReleases() {
     const headers = new HttpHeaders({
       // tslint:disable-next-line: max-line-length
-      'Authorization': 'Bearer BQCbxBXTJ8ICdADOqlp2UcvYghNVUrleJLVUiqbwS0hejPgZf5UhyfVO4Qu3bW9S1FvT_38v4akfdU06q8xzV2XigWmMmaJV9dgn_pepCahTQDQ7zErZ9MekZJEZSVoIlDZgJg_ByA'
+      'Authorization': 'Bearer BQB9ekrqFm1aXvqmo2HMIG4Qs3piUHAeCmZZ4f0sVMAJFvNBhHpAES038iZhCybIh8LhhUnl5OZcD2T06nxnmCZO-ZKaIUYPYKvf2vlfyGP682PdG_YV1s0fE-m8pJvQ7TCkcBU89w'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+            .pipe( map( (data: any) => data.albums.items));
   }
 
   getArtista(termino: string){
@@ -22,6 +24,7 @@ export class SpotifyService {
       // tslint:disable-next-line: max-line-length
       'Authorization': 'Bearer BQD8nZlZKBP3JIvI22Ifnc_8bsfEUauRsmEdWx42ek1gBjNFvIeIi8Q7FMo4IcifExOccyPLnubIP68jaWnI5DIftPHiz-oUDPe5g-EeglpExFabBsDgJ2kBATl8_MxPvexEV5mOHA'
     });
-    return this.http.get(`https://api.spotify.com/v1/search?query=${ termino }&type=artist&offset=5&limit=15`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/search?query=${ termino }&type=artist&offset=5&limit=15`, { headers })
+          .pipe( map ( (data: any) => data.artists.items));
   }
 }
